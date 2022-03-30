b11767af7dd3d5d8abae125be825b393f25321e0
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-03-30 21:17:16 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <header>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Playstation_logo_colour.svg" alt=""/>
+            </header>
+            <nav>
+                <div>Profile</div>
+            </nav>
+            <div>Main Content</div>
+        </div>
+    );
+}
+
+export default App;
