6305046d0ed3d41888023de7b2e6335cd41000f6
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-04-18 13:12:48 +0300

    add ava in dialogs

--- src/components/Dialogs/DialogsItem/DialogsItem.tsx
+++ src/components/Dialogs/DialogsItem/DialogsItem.tsx
@@ -1,6 +1,7 @@
 import c from "../Dialogs.module.css";
 import {NavLink} from "react-router-dom";
 import {DialogsDataType} from "../../../redux/state";
+import React from "react";
 
 type DialogsItemPropsType={
      dialogsData: Array<DialogsDataType>,
@@ -9,7 +10,10 @@ type DialogsItemPropsType={
 export const DialogsItem = (props:DialogsItemPropsType) => {
     let dialog = props.dialogsData.map(e => {
         let path = '/dialogs/' + e.id
-          return   < NavLink to = {path} className = {c.dialog} > {e.name}</NavLink>
+          return (<div className={c.dialog}>
+              <img src="https://cdn-0.emojis.wiki/emoji-pics/twitter/orange-circle-twitter.png" alt=""/>
+              < NavLink to={path} className={c.name}> {e.name}</NavLink>
+          </div>)
     })
     return (
         <div className={c.dialogsItem}>
