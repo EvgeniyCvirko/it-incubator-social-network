32259af7518ed90ec51ef9bd6d9343716fed4709
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-06-27 20:38:17 +0300

    refactoring

--- src/components/ProfilePage/MyPost/Block/BlockContainer.tsx
+++ src/components/ProfilePage/MyPost/Block/BlockContainer.tsx
@@ -6,21 +6,6 @@ import {connect} from "react-redux";
 import {AppStateType} from "../../../../redux/redux_store";
 import {Dispatch} from "redux";
 
-// type BlockContainerPropsType = {
-//     data: StoreType
-// }
-
-/*export const BlockContainer = (props: BlockContainerPropsType ) => {
-    let state = props.data.getState()
-    const addNewPost = (text: string | undefined) => {
-        props.data.dispatch(addPostAC(text))
-        props.data.dispatch(UpdateNewPosAC(""))
-    }
-    const onChangeHandler = (newText: string) => {
-        props.data.dispatch({type: 'Update_New_Post', newText: newText})
-    }
-    return <Block addPost={addNewPost} updateNewPost={onChangeHandler} newPostText={state.profilePage.newPostText}/>
-}*/
 type MapStateToPropsType = {
     newPostText: string
 }
