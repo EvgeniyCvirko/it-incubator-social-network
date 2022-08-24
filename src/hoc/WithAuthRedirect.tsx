b5d66bc524461eabd00c8d6d32ffa15f4f5b46f2
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-08-24 16:39:43 +0300

    lesson 80 (Initialised) WhithRedirect

--- src/hoc/WithAuthRedirect.tsx
+++ src/hoc/WithAuthRedirect.tsx
@@ -2,20 +2,24 @@ import React, {ComponentType} from 'react';
 import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import {AppStateType} from "../redux/redux_store";
+import {Loading} from "../components/common/Loading";
 
 type MapStateToPropsType = {
     isAuth: boolean
+    isAuthLoading: boolean
 }
 
 const MapStateToProps = (state: AppStateType): MapStateToPropsType => {
     return {
-        isAuth: state.auth.isAuth
+        isAuth: state.auth.isAuth,
+        isAuthLoading: state.auth.isLoading
     }
 }
 
 export function WithAuthRedirect <T>(Component: ComponentType<T>) {
     const RedirectComponent = (props: MapStateToPropsType) => {
-        let {isAuth, ...restProps} = props
+        let {isAuth,isAuthLoading, ...restProps} = props
+        if(isAuthLoading) return <Loading/>
         if (!isAuth) {
             return <Redirect to='/login'/>
         }
--- src/redux/redux_store.tsx
+++ src/redux/redux_store.tsx
@@ -1,11 +1,12 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore, Store} from "redux";
+import {Action, applyMiddleware, combineReducers, legacy_createStore as createStore, Store} from "redux";
 import {profilePageReducer} from "./ProfilePageReducer";
 import {dialogsPageReducer} from "./DialoguesPageReducer";
 import {sideBarReducer} from "./SideBarReducer";
 import {usersPageReducer} from "./UsersReducer";
 import {AuthReducer} from "./AuthReducer";
-import  thunkMiddleware from "redux-thunk";
+import  thunkMiddleware, { ThunkAction } from "redux-thunk";
 import {reducer as formReducer} from "redux-form"
+import {AppReducer} from "./AppReducer";
 
 
 
@@ -17,11 +18,12 @@ let rootReducer = combineReducers({
     usersPage: usersPageReducer,
     auth: AuthReducer,
     form: formReducer,// обязательное имея свойства store -> form
+    app: AppReducer
 });
-
+export type BaseThunkType<A extends Action,R = Promise<void>> = ThunkAction< R,AppStateType,unknown,A >
 export type AppStateType = ReturnType<typeof rootReducer>// дай мне возвращаемый тип, и возьми его из rootReducer
 
- let store: Store<AppStateType>  = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+let store: Store<AppStateType>  = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export default store
 
